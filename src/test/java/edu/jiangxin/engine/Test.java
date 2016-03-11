3bd37ef7c9ba8844bcac6d88b063618ecd6a291a
jiangxincode <jiangxinnju@163.com>
2016-03-11 21:31:43 +0800

    test

---
+++ src/test/java/edu/jiangxin/engine/Test.java
@@ -0,0 +1,18 @@
+package edu.jiangxin.engine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+public class Test {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@org.junit.Test
+	public void testGetTime() {
+
+	}
+
+}
